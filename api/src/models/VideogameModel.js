4f96ecf5831121c593a791cd07ae7f557adf938a
CabreraBrian <cabrerabriantbj@gmail.com>
2023-07-14 01:22:05 -0300

    commit comienzo frontEnd

--- api/src/models/VideogameModel.js
+++ api/src/models/VideogameModel.js
@@ -1,4 +1,4 @@
-const { DataTypes, STRING } = require('sequelize');
+const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -30,7 +30,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     rating: {
-      type: DataTypes.STRING,
+      type: DataTypes.REAL,
       allowNull: false,
     },
     genres: {
